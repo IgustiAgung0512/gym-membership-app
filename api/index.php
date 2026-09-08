1ee82366861b5c5d038db46c3c8349b3f64f0547
I Gusti Agung Ketut Agung <igustiagungketutagung37@gmail.com>
2026-09-08 20:04:15 +0700

    Fix database connection default to mysql

--- api/index.php
+++ api/index.php
@@ -34,6 +34,12 @@ putenv('APP_DEBUG=true');
 $_ENV['APP_DEBUG'] = 'true';
 $_SERVER['APP_DEBUG'] = 'true';
 
+if (empty($_ENV['DB_CONNECTION']) && empty($_SERVER['DB_CONNECTION']) && !getenv('DB_CONNECTION')) {
+    putenv('DB_CONNECTION=mysql');
+    $_ENV['DB_CONNECTION'] = 'mysql';
+    $_SERVER['DB_CONNECTION'] = 'mysql';
+}
+
 // Arahkan log ke stderr Vercel
 putenv('LOG_CHANNEL=stderr');
 $_ENV['LOG_CHANNEL'] = 'stderr';
