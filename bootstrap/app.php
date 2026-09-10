967a96bf902346055d51722536eae25528fbc343
I Gusti Agung Ketut Agung <igustiagungketutagung37@gmail.com>
2026-09-10 10:44:00 +0700

    uptdnew

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'v1',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
